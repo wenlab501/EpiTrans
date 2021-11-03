50e8b3fecbe91c2b9ad09c920a423fa198a9d4d6
杜承軒 <79490093+ChenHsuanTu@users.noreply.github.com>
2021-11-04 01:36:26 +0800

    update

--- R/plot_grid.R
+++ R/plot_grid.R
@@ -8,7 +8,7 @@
 #' @examples
 #' median_function(seq(1:10))
 
-plot_grid <- function(x, y, Rj = NULL, hex=F, grid.n=50, crs = NULL, bnd = NULL, basemap = NULL, interact = TRUE, gridLonLat = TRUE, title = ""){
+plot_grid <- function(x, y, Rj = NULL, hex = FALSE, grid.n=50, crs = NULL, bnd = NULL, basemap = NULL, interact = TRUE, gridLonLat = TRUE, title = ""){
   require("sf")
   require("tmap")
   require("tmaptools")
